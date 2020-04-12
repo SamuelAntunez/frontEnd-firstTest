bef1ebbd89b06abfec9475c36c1e7d43068b717d
samuel <samueljoseantunez@gmail.com>
2020-04-12 16:36:46 -0500

    Primer Commit development

---
+++ js/barra.js
@@ -0,0 +1,6 @@
+const botonbarra = document.querySelector('.botonbarra');
+
+botonbarra.addEventListener("click", function(){
+    console.log("clicked")
+    document.getElementById("barra").classList.toggle("active");
+});
